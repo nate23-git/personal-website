989bc8819992041206be494871dd279b4d35aa39
nate <nathan.perez02@utrgv.edu>
2026-05-29 06:24:54 -0500

    Finished homepage layout, and added project pages

---
+++ portfolio-app/src/app/projects/data-analysis/page.js
@@ -0,0 +1,15 @@
+export default function Page() {
+    return (
+        <main className="max-w-4xl mx-auto px-6 py-20 text-zinc-100">
+
+            <h1 className="text-4xl font-bold text-pink-300">
+                Data Analysis
+            </h1>
+
+            <p className="mt-6 text-zinc-300">
+                Exploratory data analysis using Python and visualization tools to uncover trends in datasets.
+            </p>
+
+        </main>
+    );
+}
